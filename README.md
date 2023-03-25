d91ffe3a4563fb78704cb14ddd87c7a0610fa4ce
lilbetter-l <18663221623@163.com>
2023-03-25 20:20:15 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# GCMFNet：这里自己论文标题
+
+
+> **Authors:** 
+> Qilun Li,
+
+## 1. Proposed Baseline
+
+### 1.1. Training/Testing
+
+The training and testing experiments are conducted using [PyTorch](https://github.com/pytorch/pytorch) with 
+a single NVIDIA GeForce GPU of 24 GB Memory.
+
+1. Configuring your environment (Prerequisites):
+   
+    + Creating a virtual environment in terminal: `conda create -n BGNet python=3.6`.
+    
+    + Installing necessary packages: `pip install -r requirements.txt`.
+
+1. Downloading necessary data:
+
+    + downloading testing dataset and move it into `./data/TestDataset/`, 
+    which can be found in this [download link (Google Drive)](https://drive.google.com/file/d/1SLRB5Wg1Hdy7CQ74s3mTQ3ChhjFRSFdZ/view?usp=sharing).
+    
+    + downloading training dataset and move it into `./data/TrainDataset/`, 
+    which can be found in this [download link (Google Drive)](https://drive.google.com/file/d/1Kifp7I0n9dlWKXXNIbN7kgyokoRY4Yz7/view?usp=sharing).
+    
+    + downloading pretrained weights and move it into `./checkpoints/best/BGNet.pth`, 
+    which can be found in this [download link (Google Drive)](https://drive.google.com/file/d/1uRsku9-ILQvNhqAYYar3O71ENhbBU1BA/view?usp=sharing).  
+    
+    + downloading Res2Net weights and move it into `./models/res2net101_v1b_26w_4s-0812c246.pth`[download link (Google Drive)](https://drive.google.com/file/d/1vLrr82_SbNY__etRcc4NuxODu13OIeLa/view?usp=sharing). 
+   
+1. Training Configuration:
+
+    + Assigning your costumed path, like `--train_save` and `--train_path` in `etrain.py`.
+
+1. Testing Configuration:
+
+    + After you download all the pre-trained model and testing dataset, just run `etest.py` to generate the final prediction map: replace your trained model directory (`--pth_path`).
+
+### 1.2 Evaluating your trained model:
+
+One-key evaluation is written in MATLAB code (revised from [link](https://github.com/DengPingFan/CODToolbox)), 
+please follow this the instructions in `./eval/main.m` and just run it to generate the evaluation results in.
+
+If you want to speed up the evaluation on GPU, you just need to use the [efficient tool](https://github.com/lartpang/PySODMetrics) by `pip install pysodmetrics`.
+
+Assigning your costumed path, like `method`, `mask_root` and `pred_root` in `eval.py`.
+
+Just run `eval.py` to evaluate the trained model.
+
+> pre-computed maps of GCMFNet can be found in [download link (Google Drive)](https://drive.google.com/file/d/141-7rrpmgj1f6tau6p8Plb2oEJCTTl79/view?usp=sharing). 
+
+
+
---
+++ net/bgnet.py
@@ -0,0 +1,363 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from net.ResNet import resnet50
+from math import log
+from net.Res2Net import res2net50_v1b_26w_4s
+from net.Res2Net import res2net101_v1b_26w_4s
+
+import math
+
+
+from timm.models.layers import to_2tuple
+
+class DepthWiseConv(nn.Module):
+    def __init__(self, in_channel, out_channel):
+        # 这一行千万不要忘记
+        super(DepthWiseConv, self).__init__()
+
+        # 逐通道卷积
+        self.depth_conv = nn.Conv2d(in_channels=in_channel,
+                                    out_channels=in_channel,
+                                    kernel_size=3,
+                                    stride=1,
+                                    padding=1,
+                                    groups=in_channel)
+        # groups是一个数，当groups=in_channel时,表示做逐通道卷积
+
+        # 逐点卷积
+        self.point_conv = nn.Conv2d(in_channels=in_channel,
+                                    out_channels=out_channel,
+                                    kernel_size=1,
+                                    stride=1,
+                                    padding=0,
+                                    groups=1)
+
+
+    def forward(self, input):
+        out = self.depth_conv(input)
+        out = self.point_conv(out)
+        return out
+
+
+class GlobalContextBlock(nn.Module):
+    def __init__(self, in_channels, scale = 16):
+        super(GlobalContextBlock, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = self.in_channels//scale
+
+        self.Conv_key = nn.Conv2d(self.in_channels, 1, 1)
+        self.SoftMax = nn.Softmax(dim=1)
+
+        self.Conv_value = nn.Sequential(
+            DepthWiseConv(self.in_channels, self.out_channels),
+            nn.BatchNorm2d(self.out_channels),
+            nn.GELU(),
+
+            DepthWiseConv(self.out_channels, self.in_channels),
+        )
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+        # key -> [b, 1, H, W] -> [b, 1, H*W] ->  [b, H*W, 1]
+        key = self.SoftMax(self.Conv_key(x).view(b, 1, -1).permute(0, 2, 1).view(b, -1, 1).contiguous())
+        query = x.view(b, c, h*w)
+        # [b, c, h*w] * [b, H*W, 1]
+        concate_QK = torch.matmul(query, key)
+        concate_QK = concate_QK.view(b, c, 1, 1).contiguous()
+        value = self.Conv_value(concate_QK)
+        out = x + value
+        return out
+
+def _get_act_fn(act_name, inplace=True):
+    if act_name == "relu":
+        return nn.ReLU(inplace=inplace)
+    elif act_name == "leaklyrelu":
+        return nn.LeakyReLU(negative_slope=0.1, inplace=inplace)
+    elif act_name == "gelu":
+        return nn.GELU()
+    else:
+        raise NotImplementedError
+
+
+class ConvBNReLU(nn.Sequential):
+    def __init__(
+            self,
+            in_planes,
+            out_planes,
+            kernel_size,
+            stride=1,
+            padding=0,
+            dilation=1,
+            groups=1,
+            bias=False,
+            act_name="relu",
+            is_transposed=False,
+    ):
+        """
+        Convolution-BatchNormalization-ActivationLayer
+
+        :param in_planes:
+        :param out_planes:
+        :param kernel_size:
+        :param stride:
+        :param padding:
+        :param dilation:
+        :param groups:
+        :param bias:
+        :param act_name: None denote it doesn't use the activation layer.
+        :param is_transposed: True -> nn.ConvTranspose2d, False -> nn.Conv2d
+        """
+        super().__init__()
+        if is_transposed:
+            conv_module = nn.ConvTranspose2d
+        else:
+            conv_module = nn.Conv2d
+        self.add_module(
+            name="conv",
+            module=conv_module(
+                in_planes,
+                out_planes,
+                kernel_size=kernel_size,
+                stride=to_2tuple(stride),
+                padding=to_2tuple(padding),
+                dilation=to_2tuple(dilation),
+                groups=groups,
+                bias=bias,
+            ),
+        )
+        self.add_module(name="bn", module=nn.BatchNorm2d(out_planes))
+        if act_name is not None:
+            self.add_module(name=act_name, module=_get_act_fn(act_name=act_name))
+
+class HMU(nn.Module):
+    def __init__(self, in_c, num_groups=4, hidden_dim=None):
+        super().__init__()
+        self.num_groups = num_groups
+
+        hidden_dim = hidden_dim or in_c // 2
+        expand_dim = hidden_dim * num_groups
+        self.expand_conv = DepthWiseConv(in_c, expand_dim)
+        self.gelu = nn.GELU()
+        self.gate_genator = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            DepthWiseConv(num_groups * hidden_dim, hidden_dim),
+            # nn.ReLU(True),
+            nn.GELU(),
+            DepthWiseConv(hidden_dim, num_groups * hidden_dim),
+            nn.Softmax(dim=1),
+        )
+
+        self.interact = nn.ModuleDict()
+        self.interact["0"] = ConvBNReLU(hidden_dim, 3 * hidden_dim, 3, 1, 1)
+        for group_id in range(1, num_groups - 1):
+            self.interact[str(group_id)] = ConvBNReLU(2 * hidden_dim, 3 * hidden_dim, 3, 1, 1)
+        self.interact[str(num_groups - 1)] = ConvBNReLU(2 * hidden_dim, 2 * hidden_dim, 3, 1, 1)
+
+        self.fuse = nn.Sequential(DepthWiseConv(num_groups * hidden_dim, in_c), nn.BatchNorm2d(in_c))
+        # self.fuse = nn.Sequential(nn.DepthWiseConv(num_groups * hidden_dim, in_c, 3, 1, 1), nn.BatchNorm2d(in_c))
+        # self.final_relu = nn.ReLU(True)
+        self.final_relu = nn.GELU()
+        
+
+    def forward(self, x):
+        # xs = self.expand_conv(x).chunk(self.num_groups, dim=1)
+        xs = self.gelu(self.expand_conv(x)).chunk(self.num_groups, dim=1)
+
+        outs = []
+
+        branch_out = self.interact["0"](xs[0])
+        outs.append(branch_out.chunk(3, dim=1))
+
+        for group_id in range(1, self.num_groups - 1):
+            branch_out = self.interact[str(group_id)](torch.cat([xs[group_id], outs[group_id - 1][1]], dim=1))
+            outs.append(branch_out.chunk(3, dim=1))
+
+        group_id = self.num_groups - 1
+        branch_out = self.interact[str(group_id)](torch.cat([xs[group_id], outs[group_id - 1][1]], dim=1))
+        outs.append(branch_out.chunk(2, dim=1))
+
+        out = torch.cat([o[0] for o in outs], dim=1)
+        gate = self.gate_genator(torch.cat([o[-1] for o in outs], dim=1))
+        out = self.fuse(out * gate)
+        return self.final_relu(out + x)
+
+
+
+
+
+
+
+class ConvBNR(nn.Module):
+    def __init__(self, inplanes, planes, kernel_size=3, stride=1, dilation=1, bias=False):
+        super(ConvBNR, self).__init__()
+
+        self.block = nn.Sequential(
+            nn.Conv2d(inplanes, planes, kernel_size, stride=stride, padding=dilation, dilation=dilation, bias=bias),
+            nn.BatchNorm2d(planes),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class Conv1x1(nn.Module):
+    def __init__(self, inplanes, planes):
+        super(Conv1x1, self).__init__()
+        self.conv = nn.Conv2d(inplanes, planes, 1)
+        self.bn = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+
+        return x
+
+
+class EAM(nn.Module):
+    def __init__(self):
+        super(EAM, self).__init__()
+        self.reduce1 = Conv1x1(256, 64)
+        self.reduce4 = Conv1x1(2048, 256)
+        self.block = nn.Sequential(
+            ConvBNR(256 + 64, 256, 3),
+            ConvBNR(256, 256, 3),
+            nn.Conv2d(256, 1, 1))
+
+    def forward(self, x4, x1):
+        size = x1.size()[2:]
+        x1 = self.reduce1(x1)
+        x4 = self.reduce4(x4)
+        x4 = F.interpolate(x4, size, mode='bilinear', align_corners=False)
+        out = torch.cat((x4, x1), dim=1)
+        out = self.block(out)
+
+        return out
+
+
+class EFM(nn.Module):
+    def __init__(self, channel):
+        super(EFM, self).__init__()
+        t = int(abs((log(channel, 2) + 1) / 2))
+        k = t if t % 2 else t + 1
+        self.conv2d = ConvBNR(channel, channel, 3)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv1d = nn.Conv1d(1, 1, kernel_size=k, padding=(k - 1) // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, c, att):
+        if c.size() != att.size():
+            att = F.interpolate(att, c.size()[2:], mode='bilinear', align_corners=False)
+        x = c * att + c
+        x = self.conv2d(x)
+        wei = self.avg_pool(x)
+        wei = self.conv1d(wei.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+        wei = self.sigmoid(wei)
+        x = x * wei
+
+        return x
+
+
+class CAM(nn.Module):
+    def __init__(self, hchannel, channel):
+        super(CAM, self).__init__()
+        self.conv1_1 = Conv1x1(hchannel + channel, channel)
+        self.conv3_1 = ConvBNR(channel // 4, channel // 4, 3)
+        self.dconv5_1 = ConvBNR(channel // 4, channel // 4, 3, dilation=2)
+        self.dconv7_1 = ConvBNR(channel // 4, channel // 4, 3, dilation=3)
+        self.dconv9_1 = ConvBNR(channel // 4, channel // 4, 3, dilation=4)
+        self.conv1_2 = Conv1x1(channel, channel)
+        self.conv3_3 = ConvBNR(channel, channel, 3)
+
+    def forward(self, lf, hf):
+        if lf.size()[2:] != hf.size()[2:]:
+            hf = F.interpolate(hf, size=lf.size()[2:], mode='bilinear', align_corners=False)
+        x = torch.cat((lf, hf), dim=1)
+        x = self.conv1_1(x)
+        xc = torch.chunk(x, 4, dim=1)
+        x0 = self.conv3_1(xc[0] + xc[1])
+        x1 = self.dconv5_1(xc[1] + x0 + xc[2])
+        x2 = self.dconv7_1(xc[2] + x1 + xc[3])
+        x3 = self.dconv9_1(xc[3] + x2)
+        xx = self.conv1_2(torch.cat((x0, x1, x2, x3), dim=1))
+        x = self.conv3_3(x + xx)
+
+        return x
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.resnet = res2net101_v1b_26w_4s(pretrained=True)
+        self.eam = EAM()
+    
+        self.gc1 = GlobalContextBlock(256, 256)
+        self.gc2 = GlobalContextBlock(512, 512)
+        self.gc3 = GlobalContextBlock(1024, 1024)
+        self.gc4 = GlobalContextBlock(2048, 2048)
+
+        self.hmu1 = HMU(64)
+        self.hmu2 = HMU(128)
+        self.hmu3 = HMU(256)
+ 
+        self.efm1 = EFM(256)
+        self.efm2 = EFM(512)
+        self.efm3 = EFM(1024)
+        self.efm4 = EFM(2048)
+
+        self.reduce1 = Conv1x1(256, 64)
+        self.reduce2 = Conv1x1(512, 128)
+        self.reduce3 = Conv1x1(1024, 256)
+        self.reduce4 = Conv1x1(2048, 256)
+
+        self.cam1 = CAM(128, 64)
+        self.cam2 = CAM(256, 128)
+        self.cam3 = CAM(256, 256)
+
+        self.predictor1 = nn.Conv2d(64, 1, 1)
+        self.predictor2 = nn.Conv2d(128, 1, 1)
+        self.predictor3 = nn.Conv2d(256, 1, 1)
+
+    def forward(self, x):
+        x1, x2, x3, x4 = self.resnet(x)
+
+        x1 = self.gc1(x1)
+        x2 = self.gc2(x2)
+        x3 = self.gc3(x3)
+        x4 = self.gc4(x4)
+
+        edge = self.eam(x4, x1)
+        edge_att = torch.sigmoid(edge)
+
+
+        x1a = self.efm1(x1, edge_att)
+        x2a = self.efm2(x2, edge_att)
+        x3a = self.efm3(x3, edge_att)
+        x4a = self.efm4(x4, edge_att)
+
+        x1r = self.reduce1(x1a)
+        x2r = self.reduce2(x2a)
+        x3r = self.reduce3(x3a)
+        x4r = self.reduce4(x4a)
+
+        x34 = self.cam3(x3r, x4r)
+        x234 = self.cam2(x2r, x34)
+        x1234 = self.cam1(x1r, x234)
+
+
+        x1234 = self.hmu1(x1234)
+        x234 = self.hmu2(x234)
+        x34 = self.hmu3(x34)
+
+        o3 = self.predictor3(x34)
+        o3 = F.interpolate(o3, scale_factor=16, mode='bilinear', align_corners=False)
+        o2 = self.predictor2(x234)
+        o2 = F.interpolate(o2, scale_factor=8, mode='bilinear', align_corners=False)
+        o1 = self.predictor1(x1234)
+        o1 = F.interpolate(o1, scale_factor=4, mode='bilinear', align_corners=False)
+        oe = F.interpolate(edge_att, scale_factor=4, mode='bilinear', align_corners=False)
+
+        return o3, o2, o1, oe
